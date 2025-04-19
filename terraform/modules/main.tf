ee375b07280b9927b2534c940b83f4b038503fa5
David StClair <david@davidstclair.co.uk>
2025-04-19 10:17:07 +0100

    Update main.tf

--- terraform/modules/main.tf
+++ terraform/modules/main.tf
@@ -15,13 +15,37 @@ locals {
   public_subnets = data.terraform_remote_state.bootstrap.outputs.public_subnets
 }
 
-module "postgres" {
-  source = "../../modules/database"
-  vpc_id = local.vpc_id
+module "bootstrap" {
+  source = ../modules/bootstrap"
+  name = local.project_name
+  environment = local.environment
+  vpc_cidr = local.vpc_cidr
+  azs = local.azs
+  tags = local.tags
+  ssm_ami = local.ssm_ami
+}
+
+module "database" {
+  source = "../modules/database"
+  vpc_id = module.outputs.vpc_id
   vpc_cidr = local.vpc_cidr
   name = local.name
   aws_account_id = local.aws_account_id
   environment = local.environment
   tags = local.tags
   aws_region = local.aws_region
+}
+
+module "cluster" {
+  source = "../modules/cluster"
+  vpc_id = module.bootstrap.outputs.vpc_id
+  name = local.name
+  aws_account_id = local.aws_account_id
+  environment = local.environment
+  tags = local.tags
+  private_subnets = module.bootstrap.outputs.private_subnets
+  public_subnets = module.bootstrap.outputs.public_subnets
+  key_owners_arn = data.aws_caller_identity.current.arn
+  aws_region = local.aws_region
+  random_string = data.terraform_remote_state.bootstrap.outputs.random_string
 }
